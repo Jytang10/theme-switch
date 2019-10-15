075d81dad0478c4923cf5ec019b4fcc445c34fd7
James Tang <jytang10@gmail.com>
2019-10-14 21:05:45 -0700

    Created reducer

---
+++ redux/themeReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  theme: {}
+}
+
+export default function(state = initialState, action){
+  switch(action.type){
+    case "SWITCH_THEME":
+      let newState = {
+        ...state,
+        theme:{...state.theme, ...action.baseTheme}
+      }
+      return newState
+
+      default:
+        return state
+  }
+}
